7ff424ef1b44c964968d46a9b826e28c732008c5
Xavier-Amatller <reyo2004@gmail.com>
2024-10-01 15:41:34 +0200

    Prueba /var/ & /vendor/

---
+++ var/cache/dev/ContainerQjUbQPJ/RequestPayloadValueResolverGhost01ca9cc.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ContainerQjUbQPJ;
+include_once \dirname(__DIR__, 4).''.\DIRECTORY_SEPARATOR.'vendor'.\DIRECTORY_SEPARATOR.'symfony'.\DIRECTORY_SEPARATOR.'http-kernel'.\DIRECTORY_SEPARATOR.'Controller'.\DIRECTORY_SEPARATOR.'ValueResolverInterface.php';
+include_once \dirname(__DIR__, 4).''.\DIRECTORY_SEPARATOR.'vendor'.\DIRECTORY_SEPARATOR.'symfony'.\DIRECTORY_SEPARATOR.'http-kernel'.\DIRECTORY_SEPARATOR.'Controller'.\DIRECTORY_SEPARATOR.'ArgumentResolver'.\DIRECTORY_SEPARATOR.'RequestPayloadValueResolver.php';
+
+class RequestPayloadValueResolverGhost01ca9cc extends \Symfony\Component\HttpKernel\Controller\ArgumentResolver\RequestPayloadValueResolver implements \Symfony\Component\VarExporter\LazyObjectInterface
+{
+    use \Symfony\Component\VarExporter\LazyGhostTrait;
+
+    private const LAZY_OBJECT_PROPERTY_SCOPES = [
+        "\0".parent::class."\0".'serializer' => [parent::class, 'serializer', parent::class],
+        "\0".parent::class."\0".'translator' => [parent::class, 'translator', parent::class],
+        "\0".parent::class."\0".'validator' => [parent::class, 'validator', parent::class],
+        'serializer' => [parent::class, 'serializer', parent::class],
+        'translator' => [parent::class, 'translator', parent::class],
+        'validator' => [parent::class, 'validator', parent::class],
+    ];
+}
+
+// Help opcache.preload discover always-needed symbols
+class_exists(\Symfony\Component\VarExporter\Internal\Hydrator::class);
+class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectRegistry::class);
+class_exists(\Symfony\Component\VarExporter\Internal\LazyObjectState::class);
+
+if (!\class_exists('RequestPayloadValueResolverGhost01ca9cc', false)) {
+    \class_alias(__NAMESPACE__.'\\RequestPayloadValueResolverGhost01ca9cc', 'RequestPayloadValueResolverGhost01ca9cc', false);
+}
---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/home' => [[['_route' => 'app_home', '_controller' => 'App\\Controller\\HomeController::index'], null, null, null, false, false, null]],
+        '/nurses' => [[['_route' => 'nursesInfo', '_controller' => 'App\\Controller\\NurseController::findall'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
