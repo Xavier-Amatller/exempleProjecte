aced18b618580770750caf59be4f4cd3a4c2570b
HodeonArtz <martidicianolold@gmail.com>
2024-10-01 16:57:25 +0200

    closes #2 Implement a login method in NurseController.php

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class NurseController extends AbstractController
@@ -19,4 +20,13 @@ class NurseController extends AbstractController
         ["nombre" => "Fernanfloo",
         "pwd" => "9876"]
     ];
+    #[Route('/nurse_login', name: 'app_home', methods: ['GET'])]
+    public function login(Request $request): JsonResponse
+    {
+      return new JsonResponse(in_array(
+        ["nombre"=> $request->get("nombre"),
+        "pwd"=>$request->get("pwd")],
+        self::$data)
+      );
+    }
 }
