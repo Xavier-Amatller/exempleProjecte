c56fc37445001c0425bf97a180d1565c11b67e24
perita23 <hugobedmar10@gmail.com>
2024-10-23 16:34:12 +0200

    All 3 endpoints finished with remote db

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -10,52 +10,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\Nurse;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Constraints\Json;
 
 #[Route('/nurse')]
 class NurseController extends AbstractController
 {
 
-
-    public $data = [
-        [
-            "nombre" => "Xavier",
-            "apellido" => "Amatller",
-            "pwd" => "1234"
-        ],
-        [
-            "nombre" => "Hugo",
-            "apellido" => "Gonzalez",
-            "pwd" => "5555"
-        ],
-        [
-            "nombre" => "Glen",
-            "apellido" => "Marti",
-            "pwd" => "0000"
-        ],
-        [
-            "nombre" => "Fernanfloo",
-            "apellido" => "Futbolista",
-            "pwd" => "9876"
-        ],
-        [
-            "nombre" => "Glen",
-            "apellido" => "Oliveres",
-            "pwd" => "2323"
-        ],
-    ];
-
-    /* 
-    #[Route('/list', name: 'app_nurse', methods: ['GET'])]
-    public function findAll(): JsonResponse
-    {
-        return new JsonResponse($this->data, 200);
-        //return $this->json($this->data);
-    }
-    */
-    
     #[Route('/list', name: 'app_nurse', methods: ['GET'])]
     public function list(EntityManagerInterface $entityManager): JsonResponse
     {
@@ -71,26 +31,8 @@ class NurseController extends AbstractController
                 $nurse->getPasswd(),
             );
         }
-        return new JsonResponse($data,JsonResponse::HTTP_OK);
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
     }
-
-    // #[Route('/login', name: 'app_nurse_login', methods: ['POST'])]
-    // public function login(Request $request): JsonResponse
-    // {
-    //     $login_success = in_array(
-    //         [
-    //             "nombre" => $request->get("nombre"),
-    //             "apellido" => $request->get("apellido"),
-    //             "pwd" => $request->get("pwd")
-    //         ],
-    //         $this->data
-    //     );
-    //     // Returns HTTP code status 200 if login is correct, 401 otherwise.
-    //     return new JsonResponse(
-    //         $login_success,
-    //         $login_success ? JsonResponse::HTTP_OK : JsonResponse::HTTP_UNAUTHORIZED
-    //     );
-    // }
 
     #[Route('/login', name: 'app_nurse_login', methods: ['POST'])]
     public function login(Request $request, EntityManagerInterface $entityManagerInterface): JsonResponse
@@ -121,25 +63,23 @@ class NurseController extends AbstractController
     {
         $name = $request->get('name') ?? null;
         $nursesFound = [];
-        
-        if (!is_null($name)) {
-          $nursesFound = $entityManager->getRepository(className: Nurse::class)->
-            findBy([
-              "name" => $name
-            ]);
 
-          $nursesFound = array_map(
-            callback: 
-              fn($nurse): mixed=> [
-                "name"=> $nurse->getName(),
-                "surname" => $nurse->getSurname()
-              ], 
-            array: $nursesFound
-          );
+        if (!is_null($name)) {
+            $nursesFound = $entityManager->getRepository(className: Nurse::class)->findBy([
+                    "name" => $name
+                ]);
+
+            $nursesFound = array_map(
+                callback: fn($nurse): mixed => [
+                    "name" => $nurse->getName(),
+                    "surname" => $nurse->getSurname()
+                ],
+                array: $nursesFound
+            );
         }
-        if (!empty($nursesFound)) 
+        if (!empty($nursesFound))
             return new JsonResponse($nursesFound, 200);
-        
+
         return new JsonResponse($nursesFound, 404);
     }
 }
